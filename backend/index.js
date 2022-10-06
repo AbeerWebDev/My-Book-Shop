31200136b26dbc80b1103567e82f261e68d6ab0a
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-10-06 15:11:10 +0400

    Updated app

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mysql from "mysql";
+import cors from 'cors';
 
 const app = express(); 
 
@@ -10,6 +11,8 @@ const db = mysql.createConnection({
     database: "test"
 })
 
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req,res) => {
 res.json("hello this is the backend")
@@ -24,16 +27,17 @@ app.get("/books", (req,res) => {
 })
 
 app.post('/books', (req,res) => {
-    const q = "INSERT INTO books (`title`. `desc`, `cover`) VALUES (?)"
+    const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)"
     const values = [
-        "title from backend",
-        "desc from backend",
-        "cover pic from backend"
+       req.body.title,
+       req.body.desc,
+       req.body.price,
+       req.body.cover,
     ];
 
     db.query(q, [values], (error, data) => {
         if (error) return res.json(error)
-        return res.json(data)
+        return res.json("book has been created successfully")
     })
 
 })
